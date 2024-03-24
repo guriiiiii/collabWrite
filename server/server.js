535fa29176009da497ebf9ec1ab4afd0f97990d6
Guri <gurtegs11@gmail.com>
2024-03-24 14:41:17 -0600

    fix

--- server/server.js
+++ server/server.js
@@ -8,10 +8,12 @@ require('dotenv').config();
 const defaultValue = "";
 
 app.use(express.json());
-app.use(cors({
-    origin: 'http://localhost:3000',
-    methods: 'GET,POST', // Adjust the methods as per your requirement
-  }));
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 mongoose.connect(process.env.MONGO_URL).then(()=>console.log(`success`)).catch((err)=>console.log(err));
 app.use("/api/docs", docRoute);

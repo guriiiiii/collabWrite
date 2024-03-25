a7bcca342627e7bd86becf96cfda2e97e666dce3
Guri <gurtegs11@gmail.com>
2024-03-24 18:59:05 -0600

    fuile name

--- client/src/config.js
+++ client/src/config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://collab-write-backend.vercel.app/api/"
+    baseURL: "https://collab-write-kappa.vercel.app/api/"
 })
 
 export default api;
